45130ed3f23e844fb76cbabeff02e2ad4ba315f7
Tkcodes0 <tkcodes0@gmail.com>
2023-12-01 13:18:37 +0300

    Class square with a private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+ Create class called square
+"""
+
+
+class Square:
+    """ This is a class with a private attribute"""
+    def __init__(self, size=None):
+        self.__size = size
