61420052b29373a776515bfca0348c75bde19ed3
Tkcodes0 <tkcodes0@gmail.com>
2023-12-01 13:36:24 +0300

    Pycodestle fix : change != to is not int

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     """ This is a class with a private attribute"""
     def __init__(self, size=0):
         self.__size = size
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
