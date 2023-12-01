85138184effa011bcefcb7d9343c963e43ba6f14
Tkcodes0 <tkcodes0@gmail.com>
2023-12-01 13:52:35 +0300

    Returns the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+ Create class called square
+"""
+
+
+class Square:
+    """ This is a class with a private attribute"""
+    def __init__(self, size=0):
+        """
+        Class method with a private attribute
+        Raises:
+                TypeError : if size is not int
+                ValueError : if size is less than 0
+        """
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """
+        This is a public class method
+        Returns: Area of the square
+        """
+        return self.__size ** 2
