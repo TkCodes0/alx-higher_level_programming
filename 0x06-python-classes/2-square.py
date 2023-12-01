bb7c13deffc90720d1d004dd2532985a9d0ffa88
Tkcodes0 <tkcodes0@gmail.com>
2023-12-01 13:33:31 +0300

    Raise TypeError and ValueError in the square class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+ Create class called square
+"""
+
+
+class Square:
+    """ This is a class with a private attribute"""
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
