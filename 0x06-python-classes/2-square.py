1b865b2891eecd158f8b4f22f70335f11645737e
Tkcodes0 <tkcodes0@gmail.com>
2023-12-01 13:39:30 +0300

    Added documentation for the class method

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,6 +8,12 @@
 class Square:
     """ This is a class with a private attribute"""
     def __init__(self, size=0):
+        """
+        Class method with a private attribute
+        Raises:
+                TypeError : if size is not int
+                ValueError : if size is less than 0
+        """
         self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
